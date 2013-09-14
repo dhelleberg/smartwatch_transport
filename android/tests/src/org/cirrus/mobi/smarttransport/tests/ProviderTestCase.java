1ca8b860f440ccd350b2c4a6aca5bccba65f2e4f
dhelleberg <helleberg@gmx.de>
2013-09-14 12:15:49 +0200

    refactored test cases

--- android/tests/src/org/cirrus/mobi/smarttransport/tests/ProviderTestCase.java
+++ android/tests/src/org/cirrus/mobi/smarttransport/tests/ProviderTestCase.java
@@ -32,6 +32,23 @@
  * along with SmartTransport.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of SmartTransport
+ *
+ * SmartTransport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SmartTransport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SmartTransport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.cirrus.mobi.smarttransport.tests;
 
 
@@ -44,6 +61,9 @@ import de.schildbach.pte.dto.LocationType;
 import de.schildbach.pte.dto.NearbyStationsResult;
 import junit.framework.Assert;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ProviderTestCase extends AndroidTestCase {
 
@@ -54,98 +74,75 @@ public class ProviderTestCase extends AndroidTestCase {
     private static final double[] LOCATION_AUGSBURG = {48.365374,10.888503};
     private static final double[] LOCATION_MUENCHEN = {48.140232,11.558335  };
     private static final double[] LOCATION_COPENHAGEN = {55.673374,12.561321};
-
-
+    private static final double[] LOCATION_VIENNA = {48.208174,16.373819};
+    private List<AbstractNetworkProvider> successProvider;
+    List<AbstractNetworkProvider>failedProvider;
 
     @Override
     protected void setUp() throws Exception {
         Log.v(">>>>>>>>>>>>>>", "SETUP");
         super.setUp();
+        //collect failed provider
+        failedProvider = new ArrayList<AbstractNetworkProvider>();
+        //collect success provider
+        successProvider = new ArrayList<AbstractNetworkProvider>();
+
     }
 
     public void testBahnProvider() throws Exception
     {
-        Location dusLocation = new Location("FakeProvider");
-        dusLocation.setLatitude(LOCATION_DUSSELDORF[0]);
-        dusLocation.setLongitude(LOCATION_DUSSELDORF[1]);
         //test resolve of stations
-        BahnProvider bahnProvider = new BahnProvider();
-        NearbyStationsResult nearbyStationsResult = bahnProvider.queryNearbyStations(convertLoction(dusLocation), 1000, 10);
-        Assert.assertNotNull("nearbyStationsResult is null", nearbyStationsResult);
-        Log.v(">>>>>", "found: " + nearbyStationsResult.stations.size() + " stations");
-        Assert.assertEquals("nearbyStationsResult is not correct",5, nearbyStationsResult.stations.size());
-        Assert.assertEquals("First Station is not DDorf HBF", "Düsseldorf Hauptbahnhof",nearbyStationsResult.stations.get(0).name);
+        BahnProvider provider = new BahnProvider();
+        checkProvider(provider, LOCATION_DUSSELDORF[0], LOCATION_DUSSELDORF[1], 5, "Düsseldorf Hauptbahnhof");
     }
 
     public void testBerlinBrandendburgProvider() throws Exception
     {
-        //test Berlin as Location
-        Location dusLocation = new Location("FakeProvider");
-        dusLocation.setLatitude(LOCATION_BERLIN[0]);
-        dusLocation.setLongitude(LOCATION_BERLIN[1]);
-        //test resolve of stations
         VbbProvider provider = new VbbProvider();
-        NearbyStationsResult nearbyStationsResult = provider.queryNearbyStations(convertLoction(dusLocation), 1000, 10);
-        Assert.assertNotNull("nearbyStationsResult is null", nearbyStationsResult);
-        Log.v(">>>>>", "found: "+nearbyStationsResult.stations.size()+" stations");
-        Assert.assertEquals("nearbyStationsResult is not correct",10, nearbyStationsResult.stations.size());
-        Assert.assertEquals("First Station is not correct", "S+U Berlin Hauptbahnhof",nearbyStationsResult.stations.get(0).name);
+        checkProvider(provider, LOCATION_BERLIN[0], LOCATION_BERLIN[1], 10, "S+U Berlin Hauptbahnhof");
     }
 
     public void testMuenchenProvider() throws Exception
     {
-        Location dusLocation = new Location("FakeProvider");
-        dusLocation.setLatitude(LOCATION_MUENCHEN[0]);
-        dusLocation.setLongitude(LOCATION_MUENCHEN[1]);
-        //test resolve of stations
         MvvProvider provider = new MvvProvider();
-        NearbyStationsResult nearbyStationsResult = provider.queryNearbyStations(convertLoction(dusLocation), 1000, 10);
-        Assert.assertNotNull("nearbyStationsResult is null", nearbyStationsResult);
-        Log.v(">>>>>", "found: "+nearbyStationsResult.stations.size()+" stations");
-        Assert.assertEquals("nearbyStationsResult is not correct",10, nearbyStationsResult.stations.size());
-        Assert.assertEquals("First Station is not correct", "Hauptbahnhof Haupthalle",nearbyStationsResult.stations.get(0).name);
+        checkProvider(provider, LOCATION_MUENCHEN[0], LOCATION_MUENCHEN[1], 10, "Hauptbahnhof Haupthalle");
     }
 
     public void testBraunschweigProvider() throws Exception
     {
-        Location dusLocation = new Location("FakeProvider");
-        dusLocation.setLatitude(LOCATION_BRAUNSCHWEIG[0]);
-        dusLocation.setLongitude(LOCATION_BRAUNSCHWEIG[1]);
-        //test resolve of stations
         BsvagProvider provider = new BsvagProvider();
-        NearbyStationsResult nearbyStationsResult = provider.queryNearbyStations(convertLoction(dusLocation), 1000, 10);
-        Assert.assertNotNull("nearbyStationsResult is null", nearbyStationsResult);
-        Log.v(">>>>>", "found: "+nearbyStationsResult.stations.size()+" stations");
-        Assert.assertEquals("nearbyStationsResult is not correct",10, nearbyStationsResult.stations.size());
-        Assert.assertEquals("First Station is not correct", "Hauptbahnhof",nearbyStationsResult.stations.get(0).name);
+        checkProvider(provider, LOCATION_BRAUNSCHWEIG[0], LOCATION_BRAUNSCHWEIG[1], 10, "Hauptbahnhof");
     }
 
     public void testBremenProvider() throws Exception
     {
-        Location dusLocation = new Location("FakeProvider");
-        dusLocation.setLatitude(LOCATION_BREMEN[0]);
-        dusLocation.setLongitude(LOCATION_BREMEN[1]);
-        //test resolve of stations
         BsagProvider provider = new BsagProvider();
-        NearbyStationsResult nearbyStationsResult = provider.queryNearbyStations(convertLoction(dusLocation), 1000, 10);
-        Assert.assertNotNull("nearbyStationsResult is null", nearbyStationsResult);
-        Log.v(">>>>>", "found: "+nearbyStationsResult.stations.size()+" stations");
-        Assert.assertEquals("nearbyStationsResult is not correct",10, nearbyStationsResult.stations.size());
-        Assert.assertEquals("First Station is not correct", "Hauptbahnhof (Central Station)",nearbyStationsResult.stations.get(0).name);
+        checkProvider(provider, LOCATION_BREMEN[0], LOCATION_BREMEN[1], 10, "Hauptbahnhof (Central Station)");
+
     }
 
     public void testDenmarkProvider() throws Exception
     {
-        Location dusLocation = new Location("FakeProvider");
-        dusLocation.setLatitude(LOCATION_COPENHAGEN[0]);
-        dusLocation.setLongitude(LOCATION_COPENHAGEN[1]);
-        //test resolve of stations
         DsbProvider provider = new DsbProvider();
+        checkProvider(provider, LOCATION_COPENHAGEN[0], LOCATION_COPENHAGEN[1], 10, "Hovedbanegården (bus)");
+    }
+
+    public void testOebbProvider() throws Exception
+    {
+        OebbProvider provider = new OebbProvider();
+        checkProvider(provider, LOCATION_VIENNA[0], LOCATION_VIENNA[1], 10, "Wien Stephansplatz (Schulerstraße)");
+    }
+
+
+    private void checkProvider(AbstractNetworkProvider provider, double lat, double lon, int expectedStations, String expectedStation) throws Exception {
+        Location dusLocation = new Location("FakeProvider");
+        dusLocation.setLatitude(lat);
+        dusLocation.setLongitude(lon);
         NearbyStationsResult nearbyStationsResult = provider.queryNearbyStations(convertLoction(dusLocation), 1000, 10);
-        Assert.assertNotNull("nearbyStationsResult is null", nearbyStationsResult);
-        Log.v(">>>>>", "found: "+nearbyStationsResult.stations.size()+" stations");
-        Assert.assertEquals("nearbyStationsResult is not correct",10, nearbyStationsResult.stations.size());
-        Assert.assertEquals("First Station is not correct", "Hovedbanegården (bus)",nearbyStationsResult.stations.get(0).name);
+        Assert.assertNotNull("nearbyStationsResult is null provider: "+provider.getClass(), nearbyStationsResult);
+        Assert.assertEquals("nearbyStationsResult is not correct " + provider.getClass(), expectedStations, nearbyStationsResult.stations.size());
+        Assert.assertEquals("First Station is not correct" + provider.getClass(), expectedStation, nearbyStationsResult.stations.get(0).name);
+
     }
 
 
