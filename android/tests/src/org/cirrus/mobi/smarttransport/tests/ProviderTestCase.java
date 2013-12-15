475450bbecbe838a6f5d8a0b6df0a51da5279ac3
dhelleberg <helleberg@gmx.de>
2013-12-15 21:27:10 +0100

    disabled VOR Provider (location search seems to be broken

--- android/tests/src/org/cirrus/mobi/smarttransport/tests/ProviderTestCase.java
+++ android/tests/src/org/cirrus/mobi/smarttransport/tests/ProviderTestCase.java
@@ -49,6 +49,23 @@
  * along with SmartTransport.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of SmartTransport
+ *
+ * SmartTransport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SmartTransport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SmartTransport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.cirrus.mobi.smarttransport.tests;
 
 
@@ -146,11 +163,13 @@ public class ProviderTestCase extends AndroidTestCase {
         checkProvider(provider, LOCATION_VIENNA[0], LOCATION_VIENNA[1], 10, "Wien Stephansplatz (Schulerstraße)");
     }
 
+/*
     public void testVorProvider() throws Exception
     {
         VorProvider provider = new VorProvider();
         checkProvider(provider, LOCATION_VIENNA[0], LOCATION_VIENNA[1], 10, "Stephansplatz");
     }
+*/
 
     public void testSeProvider() throws Exception
     {
