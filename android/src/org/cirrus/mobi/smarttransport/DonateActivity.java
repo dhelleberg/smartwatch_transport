1b7954d290e63aa7c23a23243bc11bc0d4def814
dhelleberg <helleberg@gmx.de>
2013-12-20 23:20:12 +0100

    FIxed in app payment

--- android/src/org/cirrus/mobi/smarttransport/DonateActivity.java
+++ android/src/org/cirrus/mobi/smarttransport/DonateActivity.java
@@ -32,6 +32,23 @@
  * along with SmartTransport.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * This file is part of SmartTransport
+ *
+ * SmartTransport is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SmartTransport is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SmartTransport.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.cirrus.mobi.smarttransport;
 
 import android.app.Activity;
@@ -197,7 +214,7 @@ public class DonateActivity extends Activity implements OnItemSelectedListener {
 			{
 				//consume purchase
 				Log.e(TAG, "now consuming purchase...");
-				mHelper.consumeAsync(mInventory.getPurchase(info.getSku()),  consumeListener);
+				mHelper.consumeAsync(info,  consumeListener);
 			}
 
 		}
